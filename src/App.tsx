050343c91c825c87f5f44a1153033e9758665b8e
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-02-16 11:37:39 +0000

    added a counter

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,36 @@ const difficulty = ['easy', 'ok', 'difficult', 'too difficult']
 
 function CompiledButton() {
   const [isClicked, setClicked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  function clickEvent() {
+    setClicked(!isClicked)
+    setCount(count + 1)
+  }
   return (
     <div>
       {difficulty.map((n: string) =>
         <button className={isClicked ? "test" : "nothing"}
-          onClick={() => setClicked(!isClicked)}>{n}</button>)}
+          onClick={clickEvent}>{n}</button>)}
+      <p>you clicked {count} times</p>
     </div>
   )
 }
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <button onClick={() => setCount(count + 1)}>test counter</button>
+//       <p>you clicked {count} times</p>
+//     </div>
+//   )
+// }
+
 
 function App() {
   return (
     <div>
       <CompiledButton />
-
     </div>
   );
 }
